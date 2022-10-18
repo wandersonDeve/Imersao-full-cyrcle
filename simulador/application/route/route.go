7c373012df6627feb5fd64dc837476ceded98ef2
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-18 01:28:26 -0300

    feat: implements apache-kafka

--- simulador/application/route/route.go
+++ simulador/application/route/route.go
@@ -10,46 +10,53 @@ import (
 )
 
 type Route struct {
-	ID       string
-	ClientID string
-	Position []Position
+	ID        string     `json:"routeId"`
+	ClientID  string     `json:"clientId"`
+	Positions []Position `json:"position"`
 }
 
 type Position struct {
-	Lat  float64
-	Long float64
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+type PartialRoutePosition struct {
+	ID       string    `json:"routeId"`
+	ClientID string    `json:"clientId"`
+	Position []float64 `json:"position"`
+	Finished bool      `json:"finished"`
+}
+
+func NewRoute() *Route {
+	return &Route{}
 }
 
 func (r *Route) LoadPositions() error {
 	if r.ID == "" {
-		return errors.New(text:"Route id not specified")
+		return errors.New("route id not informed")
 	}
-
-	f, err := os.Open(name: "destinations/" + r.ID + ".txt")
+	f, err := os.Open("destinations/" + r.ID + ".txt")
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		data := scanner.Split(scanner.Text(), sep:",")
-		lat, err := strconv.ParseFloat(data[0], bitSize:64)
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
 		if err != nil {
 			return nil
 		}
-		long, err := strconv.ParseFloat(data[1], bitSize:64)
+		long, err := strconv.ParseFloat(data[1], 64)
 		if err != nil {
 			return nil
 		}
-		r.Position = append(r.Position, Position{
-			Lat: lat,
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
 			Long: long,
 		})
 	}
-
-	retunr nil
-
+	return nil
 }
 
 func (r *Route) ExportJsonPositions() ([]string, error) {
